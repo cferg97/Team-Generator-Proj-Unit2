a783b1952d245fb93afe3852c2d2c82a6802eb35
Catriona Ferguson <114424301+cferg97@users.noreply.github.com>
2022-10-28 11:26:53 +0100

    reset button

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 let count = 1
 const totalCount = document.getElementById("counter")
+let arrayOfNames = []
 
 
 document.getElementById("add").onclick = function () {
     let text = document.getElementById("member").value 
-    let li = "<li>" + text + "</li>"
-    document.getElementById("list-obj").innerHTML += li
-    document.getElementById("member").value = ""
+    if (text == ""){
+        return;
+    }
+    else{
+        let li = "<li>" + text + "</li>"
+        document.getElementById("list-obj").innerHTML += li
+        document.getElementById("member").value = ""
+    }
 }
 
 document.getElementById("plus").onclick = function () {
@@ -22,6 +28,15 @@ document.getElementById("minus").onclick = function () {
     count--
     totalCount.innerHTML = count
     }
+}
 
 
+document.getElementById("assign").onclick = function () {
+    let list = document.getElementById("list")
+    
+}
+
+document.getElementById("reset").onclick = function() {
+    location.reload();
+    return false;
 }
